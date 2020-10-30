8d9b042a591fbd1cfa6585d5241aa420c1afa97f
manel-bp <manelbp4@gmail.com>
2020-10-30 13:39:15 +0100

    generalitzacio de scrapping per anys 2014-2020 i estructura de classes

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# Readme title
+
+## Readme subtitle
+
+Readme text
+
+TODO:
+- get position
+- folder structure
+- fill readme
--- main.py
+++ main.py
@@ -3,32 +3,66 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 
+class Stats:
+	def __init__(self, statistics):
+		self.points = statistics[0].get_text()
+		self.played = statistics[1].get_text()
+		self.won = statistics[2].get_text()
+		self.drawn = statistics[3].get_text()
+		self.lost = statistics[4].get_text()
+		self.gf = statistics[5].get_text()
+		self.ga = statistics[6].get_text()
+		self.gd = int(self.gf) - int(self.ga)
+
+
+class Team:
+	def __init__(self, season, name, statistics):
+		self.season = season
+		# self.position = position
+		self.name = name
+		self.total = Stats(statistics[0:7])
+		self.home = Stats(statistics[7:14])
+		self.away = Stats(statistics[14:21])
+
+	def export(self):
+		# TODO: Returns a string containing all the team information to be loaded into a CSV file
+		pass
+
+
 base_url_start = "https://resultados.as.com/resultados/futbol/primera/"
 base_url_end = "/clasificacion/#"
 
-#start_year = 2001
-start_year = 2020
+all_teams_statistics = list()
+
+# start_year = 2001
+start_year = 2014
 
 if datetime.today().month >= 8:
 	end_year = datetime.today().year + 1
 else:
 	end_year = datetime.today().year
 
-
 current_year = start_year
 
-#while current_year < end_year:
-	# Fer l'extracció
-page = requests.get(base_url_start + str(current_year) + "_" + str(current_year + 1) + base_url_end)
+while current_year < end_year:
+	season = str(current_year) + "_" + str(current_year + 1)
+	print("Season {}".format(season))
 
-soup = BeautifulSoup(page.content)
+	page = requests.get(base_url_start + season + base_url_end)
+	soup = BeautifulSoup(page.content)
+	table = soup.find_all("table", class_="tabla-datos")[0].find_all('tbody')[0]
 
-table = soup.find_all("table", class_="tabla-datos")[0].find_all('tbody')[0]
+	team_names = table.find_all('th')
+	statistics = table.find_all('td')
 
-for team_name in table.find_all('th'):
-	print(team_name.find("span", class_="nombre-equipo").get_text())
+	num_values = int(len(statistics)/len(team_names))
 
-for td in table.find_all('td'):
-	print(td.get_text())
+	for i in range(len(team_names)):
+		team_name = team_names[i].find("span", class_="nombre-equipo").get_text()
+		current_statistics = statistics[num_values*i:num_values*(i+1)]
+		team = Team(season, team_name, current_statistics)
+		print("{}: {} points".format(team.name, team.total.points))
+		all_teams_statistics.append(team)
+	
+	current_year += 1
 
-#	current_year += 1
