62e18a64d3af3132905a5891f755a3acfd50e4a4
Felix Ribera <felix_ribera94@hotmail.com>
2020-11-02 17:27:35 +0100

    Afegit strip al guardat de les dades ja que hi hem trobat salts de linia.

--- utils/custom_classes.py
+++ utils/custom_classes.py
@@ -1,23 +1,21 @@
-import pandas as pd
-
 
 class Stats:
 	def __init__(self, statistics):
-		self.points = statistics[0].get_text()
-		self.played = statistics[1].get_text()
-		self.won = statistics[2].get_text()
-		self.drawn = statistics[3].get_text()
-		self.lost = statistics[4].get_text()
-		self.gf = statistics[5].get_text()
-		self.ga = statistics[6].get_text()
+		self.points = statistics[0].get_text().strip()
+		self.played = statistics[1].get_text().strip()
+		self.won = statistics[2].get_text().strip()
+		self.drawn = statistics[3].get_text().strip()
+		self.lost = statistics[4].get_text().strip()
+		self.gf = statistics[5].get_text().strip()
+		self.ga = statistics[6].get_text().strip()
 		self.gd = int(self.gf) - int(self.ga)
 
 
 class Team:
 	def __init__(self, season, name, position, statistics):
-		self.season = season
-		self.name = name
-		self.position = position
+		self.season = season.strip()
+		self.name = name.strip()  # Strip as there's break lines in both sides.
+		self.position = position.strip()
 		self.total = Stats(statistics[0:7])
 		self.home = Stats(statistics[7:14])
 		self.away = Stats(statistics[14:21])
@@ -26,7 +24,7 @@ class Team:
 	# season,name,position,points,played,won,drawn,lost,gf,ga,gd,h_points,h_played,h_won,h_drawn,h_lost,h_gf,h_ga,h_gd,a_points,a_played,a_won,a_drawn,a_lost,a_gf,a_ga,a_gd
 	def export(self):
 		team_list = list()
-		team_list.extend((self.season,self.name,self.position,self.total.points,self.total.played,self.total.won,self.total.drawn,self.total.lost,self.total.gf,self.total.ga,self.total.gd,self.home.points,self.home.played,self.home.won,self.home.drawn,self.home.lost,self.home.gf,self.home.ga,self.home.gd,self.away.points,self.away.played,self.away.won,self.away.drawn,self.away.lost,self.away.gf,self.away.ga,self.away.gd))
+		team_list.extend((self.season, self.name, self.position, self.total.points, self.total.played, self.total.won, self.total.drawn, self.total.lost, self.total.gf, self.total.ga, self.total.gd, self.home.points, self.home.played, self.home.won, self.home.drawn, self.home.lost, self.home.gf, self.home.ga, self.home.gd, self.away.points, self.away.played, self.away.won, self.away.drawn, self.away.lost, self.away.gf, self.away.ga, self.away.gd))
 
 		return team_list
 
