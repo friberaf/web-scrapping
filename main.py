847a830074c5d8d7cc938f9cf3a17910717aca50
Felix Ribera <felix_ribera94@hotmail.com>
2020-11-02 17:29:23 +0100

    Implementada la recuperació de dades del format antic. Output amb path relatiu.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 from utils.custom_classes import Team
 import csv
-import pandas as pd
+import pathlib
 
 def scraping():
 	base_url_start = "https://resultados.as.com/resultados/futbol/primera/"
@@ -12,8 +12,7 @@ def scraping():
 
 	all_teams_statistics = list()
 
-	# start_year = 2001
-	start_year = 2020
+	start_year = 2001
 
 	if datetime.today().month >= 8:
 		end_year = datetime.today().year + 1
@@ -24,10 +23,10 @@ def scraping():
 
 	while current_year < end_year:
 		season = str(current_year) + "_" + str(current_year + 1)
-		# print("Season {}".format(season))
+		print("Retrieving season {} information".format(season))
 
 		page = requests.get(base_url_start + season + base_url_end)
-		soup = BeautifulSoup(page.content)
+		soup = BeautifulSoup(page.content, 'html.parser')
 
 		if current_year > 2013:
 			teams = new_format_team_info(soup, season)
@@ -57,7 +56,6 @@ def new_format_team_info(soup, season):
 		current_statistics = statistics[num_values * i:num_values * (i + 1)]
 		current_position = positions[i].get_text()
 		team = Team(season, team_name, current_position, current_statistics)
-		print("{}. {}: {} points".format(team.position, team.name, team.total.points))
 		teams.append(team)
 
 	return teams
@@ -65,29 +63,22 @@ def new_format_team_info(soup, season):
 
 def old_format_team_info(soup, season):
 	teams = []
-	table = soup.find_all("table", class_="tabla-datos")[0].find_all('tbody')[0]
+	table_rows = soup.find_all("table", class_="clasi-grup clasi-comp")[0].find_all('tbody')[0].find_all('tr')
 
-	team_names = table.find_all('th')
-	statistics = table.find_all('td')
-	positions = table.find_all("span", class_="pos")
-
-	num_values = int(len(statistics) / len(team_names))
-
-	for i in range(len(team_names)):
-		team_name = team_names[i].find("span", class_="nombre-equipo").get_text()
-		current_statistics = statistics[num_values * i:num_values * (i + 1)]
-		current_postition = positions[i].get_text()
-		team = Team(season, team_name, current_postition, current_statistics)
-		# print("{}. {}: {} points".format(team.position, team.name, team.total.points))
+	for row in table_rows:
+		position = row.find_all("span", class_="pos")[0].get_text()
+		team_name = row.find_all("a")[0].get_text()
+		statistics = row.find_all("td")[1:]
+		team = Team(season, team_name, position, statistics)
 		teams.append(team)
 
 	return teams
 
 
 def export(teams):
-	with open('ouput.csv', mode='w', newline='') as teams_file:
+	with open('{}\\output\\team_data.csv'.format(pathlib.Path(__file__).parent), mode='w', newline='') as teams_file:
 		teams_writer = csv.writer(teams_file, delimiter=",")
-		teams_writer.writerow(['season','name','position','points','played','won','drawn','lost','gf','ga','gd','h_points','h_played','h_won','h_drawn','h_lost','h_gf','h_ga','h_gd','a_points','a_played','a_won','a_drawn','a_lost','a_gf','a_ga','a_gd'])
+		teams_writer.writerow(['season', 'name', 'position', 'points', 'played', 'won', 'drawn', 'lost', 'gf', 'ga', 'gd', 'h_points', 'h_played', 'h_won', 'h_drawn', 'h_lost', 'h_gf', 'h_ga', 'h_gd', 'a_points', 'a_played', 'a_won', 'a_drawn', 'a_lost', 'a_gf', 'a_ga', 'a_gd'])
 
 		for team in teams:
 			teams_writer.writerow(team.export())
